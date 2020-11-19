087fd74d69338e6b87315c81bafcfa16998fba72
조동휘 <pari0130@jinotech.com>
2020-11-19 13:33:37 +0900

    study 2020-11-19 01

---
+++ src/01/SingletonPattern/case1/step1/Database.java
@@ -0,0 +1,18 @@
+package case1.step1;
+
+public class Database {
+    private String name;
+
+    public Database(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
